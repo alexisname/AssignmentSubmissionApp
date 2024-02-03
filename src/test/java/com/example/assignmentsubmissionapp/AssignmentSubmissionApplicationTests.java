3b7ac02452efe274dd3a1515281b883e7eaf47c2
jc <alexchen1990@hotmail.com>
2024-02-02 19:36:07 -0800

    Rebuild with Maven, connect to MySQL, create user and assignment table

---
+++ src/test/java/com/example/assignmentsubmissionapp/AssignmentSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignmentsubmissionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentSubmissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
